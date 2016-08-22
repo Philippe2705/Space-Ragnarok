c584f386e9266935e5c53f0db1e0e11b01fd74e8
Phyronnaz <phyronnaz@gmail.com>
2016-08-22 23:48:15 +0200

    ServerConf

---
+++ Assets/Script/MainMenu/ServerConf.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerConf : MonoBehaviour
+{
+    public Slider BotCountSlider;
+    public Slider ClientCountSlider;
+}
--- Assets/Script/StaticScript.cs
+++ Assets/Script/StaticScript.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class StaticScript : MonoBehaviour
 {
 
+    int playerCount;
+    int botCount;
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -18,7 +21,22 @@ public class StaticScript : MonoBehaviour
         switch (SceneManager.GetActiveScene().name)
         {
             case "MainMenu":
+                FindObjectOfType<ServerConf>().BotCountSlider.onValueChanged.AddListener(OnBotCountChanged);
+                FindObjectOfType<ServerConf>().ClientCountSlider.onValueChanged.AddListener(OnPlayerCountChanged);
                 break;
+            case "Battle":
+                break;
+
         }
+    }
+
+    public void OnBotCountChanged(float number)
+    {
+        botCount = (int)number;
+    }
+
+    public void OnPlayerCountChanged(float number)
+    {
+        playerCount = (int)number;
     }
 }
