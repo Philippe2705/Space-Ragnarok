6450537ecbcaf6a1115cead58015d787446cdaea
dimitri2705 <philippemaquin2705@gmail.com>
2016-08-23 10:34:51 +0200

    Project Setting --> Analytics and purchases

---
+++ Assets/Script/AnalyticsScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Analytics;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Purchasing;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        Purchase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    
+
+    public void Purchase() //test
+    {
+        print("purchase...");
+        Analytics.Transaction("yolo", 0.99m, "EUR", null, null);
+    }
+}
