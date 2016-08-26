fc83f88c7f342f719548b6f8863fa854c5315341
dimitri2705 <philippemaquin2705@gmail.com>
2016-08-26 11:35:02 +0200

    Erase data button + settings menu reworked

---
+++ Assets/Script/GooglePlayServices.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using System.Collections;
+
+public class GooglePlayServices : MonoBehaviour {
+
+    public string leaderBoard = "CgkIr9XZ1cgTEAIQBg";
+    public string[] sucess = new string[] { "CgkIr9XZ1cgTEAIQAQ" /*First Ship*/, "CgkIr9XZ1cgTEAIQAg" /*A warrior begining*/, "CgkIr9XZ1cgTEAIQAw"/*War machine*/, "CgkIr9XZ1cgTEAIQBA"/*Ragnarok*/, "CgkIr9XZ1cgTEAIQBQ"/*Tornado*/ };
+
+	// Use this for initialization
+	void Start () {
+        PlayGamesPlatform.Activate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
